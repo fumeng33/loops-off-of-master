d8b08cf19e6d919467d7b813a6a8a217670fa7a5
Meng Fu <Ellie@Ellies-MacBook-Air.local>
2021-02-10 21:52:44 -0600

    find HW

---
+++ findfromscratch.js
@@ -0,0 +1,22 @@
+
+
+//find() from scratch assignment
+
+let ageArray = [24, 43, 55, 86]
+
+
+let findAge = function (array, func) {
+  // for loop to loop through each element of the passed in array
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i]
+    // if the passed in function returns true from the element, return that element
+    if (func(element, i)) {
+      return element
+    }
+    // else the function will trturn as undefined
+  }
+}
+// 
+let found = ageArray.find(element => element > 55)
+
+console.log(found)
